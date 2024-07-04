cb6e4c8c90c905d3e4f74a4a8afca6103778394d
Thiago Amaral <thiago.amaral@clover.com>
2024-07-04 10:59:31 +0100

    my first commit

---
+++ src/main/java/com/syscode/saas_erp/SaasErpApplication.java
@@ -0,0 +1,13 @@
+package com.syscode.saas_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syscode/saas_erp/SaasErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscode.saas_erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
