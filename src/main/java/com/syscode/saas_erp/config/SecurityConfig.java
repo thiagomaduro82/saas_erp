c61b52696cbf93660aec64533cce65fefc73965a
Thiago Amaral <thiago.amaral@clover.com>
2024-08-26 12:06:43 +0100

    refactor: update application properties and security

--- src/main/java/com/syscode/saas_erp/config/SecurityConfig.java
+++ src/main/java/com/syscode/saas_erp/config/SecurityConfig.java
@@ -12,10 +12,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
   private final SecurityFilter securityFilter;
 
@@ -44,4 +46,8 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
 }
